ffead3d31b52052e6a09ff4819c5f8504f529d2e
Matt Clarke <matt.clarke@esss.se>
2021-02-22 08:15:33 +0100

    Added approval test and approved result

--- src/test/java/com/adaptionsoft/games/trivia/ApprovalTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/ApprovalTest.java
@@ -1,13 +1,34 @@
 package com.adaptionsoft.games.trivia;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class ApprovalTest {
     @Test
-    public void testJson()
-    {
-        Person hero = new Person("jayne", "cobb", true, 38);
-        Approvals.verifyAsJson(hero);
+    public void testRunningGame() throws FileNotFoundException {
+        File outputFile = new File("stdout_log.txt");
+        System.setOut(new PrintStream(outputFile));
+
+        GameRunner.main(null);
+
+        String result = readStdOutputLog(outputFile);
+
+        Approvals.verify(result);
+    }
+
+    private String readStdOutputLog(File outputFile) throws FileNotFoundException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        Scanner myReader = new Scanner(outputFile);
+        while (myReader.hasNextLine()) {
+            resultStringBuilder.append(myReader.nextLine()).append("\n");
+        }
+        myReader.close();
+        return resultStringBuilder.toString();
     }
 }
