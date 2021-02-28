df30f9dda824fc256b8caa466b2eedfd23d0fb39
Matt Clarke <matt.clarke@esss.se>
2021-02-28 08:30:42 +0100

    Approval test for rolling a three

--- src/test/java/com/adaptionsoft/games/trivia/ApprovalTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/ApprovalTest.java
@@ -93,6 +93,20 @@ public class ApprovalTest {
         Approvals.verify(result);
     }
 
+    @Test
+    public void onlyPlayerRollsAThree() throws FileNotFoundException {
+        File outputFile = new File("stdout_capture.txt");
+        System.setOut(new PrintStream(outputFile));
+
+        Game game = new Game();
+        game.add("Matt");
+        game.roll(3);
+
+        String result = readStdOutputLog(outputFile);
+
+        Approvals.verify(result);
+    }
+
     private String readStdOutputLog(File outputFile) throws FileNotFoundException {
         StringBuilder resultStringBuilder = new StringBuilder();
         Scanner myReader = new Scanner(outputFile);
